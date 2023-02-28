64ceeec1c3fbc24f1bf6f09289e3f4f722dde935
mikemccomb <mikemccomb@gmail.com>
2023-02-28 15:10:08 -0600

    One contact and All contacts rendering as json

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,11 @@
 class ContactsController < ApplicationController
   def one_contact
-    contact = Contact.first
-    render json: {
-      first_name: contact.first_name,
-      last_name: contact.last_name,
-      email: contact.email,
-      phone_number: contact.phone_number,
-    }
+    @contact = Contact.first
+    render template: "contacts/show"
   end
 
   def all_contacts
-    contact = Contact.all
-    render json: contact.each do |contact|
-    end
+    @contacts = Contact.all
+    render template: "contacts/index"
   end
 end
