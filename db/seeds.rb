9d4ff18ceca4a223eb9001206f94b376b851fb77
mikemccomb <mikemccomb@gmail.com>
2023-03-01 12:19:27 -0600

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+contact = Contact.new(first_name: "Jane", last_name: "Smith", email: "jane@smith.com", phone_number: "555-212-8778")
+contact.save
+contact = Contact.new(first_name: "Guy", last_name: "Incognito", email: "who@me.com", phone_number: "555-365-4569")
+contact.save
